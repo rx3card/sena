50f9eeb1a06e9e7644f2f3990519971757b6efa8
rx3card <rx3card@gmail.com>
2026-09-18 09:57:59 -0500

    Ejercicios python

--- IV Trimestre/python/Ejercicios repetitivos/7.py
+++ IV Trimestre/python/Ejercicios repetitivos/7.py
@@ -1,11 +1,35 @@
-#Autos por color de calcomania según último dígito de la placa.
+totalAutos = int(input("Ingrese la cantidad de autos que entran a Ibague: "))
 
-c=int(input("Ingrese la cantidad de autos que entran a Ibagué: "))
+cantAmarilla = 0
+cantRosa = 0
+cantRoja = 0
+cantVerde = 0
+cantAzul = 0
 
-cA=0
-cR=0
-cV=0
-cA=0
+for contador in range(1, totalAutos + 1):
 
-for i in range(c):
-    p=int(input())
+    ultimoDigito = int(input(f"Ingrese el ultimo digito de la placa del auto {contador}: "))
+
+    if ultimoDigito == 1 or ultimoDigito == 2:
+        cantAmarilla += 1
+
+    elif ultimoDigito == 3 or ultimoDigito == 4:
+        cantRosa += 1
+
+    elif ultimoDigito == 5 or ultimoDigito == 6:
+        cantRoja += 1
+
+    elif ultimoDigito == 7 or ultimoDigito == 8:
+        cantVerde += 1
+
+    elif ultimoDigito == 9 or ultimoDigito == 0:
+        cantAzul += 1
+
+    else:
+        print("Digito invalido.")
+
+print(f"Autos con calcomania Amarilla: {cantAmarilla}")
+print(f"Autos con calcomania Rosa: {cantRosa}")
+print(f"Autos con calcomania Roja: {cantRoja}")
+print(f"Autos con calcomania Verde: {cantVerde}")
+print(f"Autos con calcomania Azul: {cantAzul}")
