0ead491b8e93b70cc149a00d28f4b4d1d6a665dd
rx3card <rx3card@gmail.com>
2026-09-18 10:19:02 -0500

    Ejercicios python

---
+++ IV Trimestre/python/Ejercicios condicionales/5.py
@@ -0,0 +1,5 @@
+x=int(input("Ingrese un numero para conocer si es par o impar: "))
+if x % 2 == 0:
+    print(f"{x} ES PAR")
+else:
+    print(f"{x} ES IMPAR.")
---
+++ IV Trimestre/python/Ejercicios condicionales/6.py
@@ -0,0 +1,6 @@
+x = float(input("Ingrese su NOTA: "))
+
+if x <= 70:
+    print("DESAPROBADO")
+elif x >= 71 and x <= 100:
+    print("APROBADO")
